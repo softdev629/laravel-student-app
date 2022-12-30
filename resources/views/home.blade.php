42175ce06ed29fcdc4ef2190eca6b7cfa86aeb71
violetstar701 <violetstar701@gmail.com>
2022-12-30 09:51:15 +0300

    padding change in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg text-center">
         @for ($i = 0; $i < 5; $i++)
-          <a href={{ "/students?grade=".($i + 1) }} style="display: inline-block; width: 50%; margin: 10px; text-align: center; font-size: 24px; padding: 24px; border: 1px solid; background-color: lightgrey;">Grade x{{ $i + 1 }}</a>    
+          <a href={{ "/students?grade=".($i + 1) }} style="display: inline-block; width: 50%; margin: 20px; text-align: center; font-size: 24px; padding: 24px; border: 1px solid; background-color: lightgrey;">Grade x{{ $i + 1 }}</a>    
         @endfor
       </div>
     </div>
